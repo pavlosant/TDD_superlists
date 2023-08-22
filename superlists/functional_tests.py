bc1deebeb2c04c45acec31dbd526928de82b489a
pantoniou <pantoniou@illumina.com>
2023-08-22 17:41:13 +0100

    upated funcional test with unittest# Please enter the commit message for your changes. Lines starting

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,36 @@
 from selenium import webdriver
+import unittest
 
-browser= webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'install' in browser.title
+    def setUp(self): 
+        self.browser= webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        # A new app for TODO Lists has the following homepage
+        self.browser.get('http://localhost:8000')
+
+        #The page title and header mention to-do lists
+        #assert 'To-Do' in browser.title
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+        #User is asked to enter a to-do item straight away
+        #User types "Buy peacock feathers"
+
+        #When User press enters the page updates and now the page lists 
+        #"1.Buy peacock feathers" as an item in a todo list
+
+        #Theres is still a text box inviting for another item 
+        #User enters whatever as second item
+        #The page updates with BOTH items in list
+
+        #Does the site remember the list?
+    
+if __name__ =='__main__':
+        unittest.main(warnings='ignore')
+        
+
