57b71ba7649d7e2c8c49934f506c215c57af6a09
pantoniou <pantoniou@illumina.com>
2023-08-23 15:46:39 +0100

    functional test now checks we can input a to-do item

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
+
 
 class NewVisitorTest(unittest.TestCase):
 
-    def setUp(self): 
-        self.browser= webdriver.Firefox()
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
@@ -14,23 +17,36 @@ class NewVisitorTest(unittest.TestCase):
         # A new app for TODO Lists has the following homepage
         self.browser.get('http://localhost:8000')
 
-        #The page title and header mention to-do lists
-        #assert 'To-Do' in browser.title
+        # The page title and header mention to-do lists
+        # assert 'To-Do' in browser.title
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
-        #User is asked to enter a to-do item straight away
-        #User types "Buy peacock feathers"
+        header_text = self.browser.find_element("tag name", 'h1').text
+        self.assertIn('To-Do', header_text)
 
-        #When User press enters the page updates and now the page lists 
-        #"1.Buy peacock feathers" as an item in a todo list
+        # User is asked to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), 'Enter a to-do item'
+        )
 
-        #Theres is still a text box inviting for another item 
-        #User enters whatever as second item
-        #The page updates with BOTH items in list
+        # User types "Buy peacock feathers"
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        # When User press enters the page updates and now the page lists
+        # "1.Buy peacock feathers" as an item in a todo list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
-        #Does the site remember the list?
-    
-if __name__ =='__main__':
-        unittest.main(warnings='ignore')
-        
+        # Theres is still a text box inviting for another item
+        # User enters whatever as second item
+        self.fail("Finish the test!")
+        # The page updates with BOTH items in list
 
+        # Does the site remember the list?
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
